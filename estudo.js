bb0a441c67960f0495689dd6d8473babaf73916a
victorhugonegri <110132707+victorhugonegri@users.noreply.github.com>
2022-09-28 18:33:05 -0300

    28/09/22

--- estudo.js
+++ estudo.js
@@ -1,7 +1,20 @@
 alert("Oi, eu sou um alerta")
         console.log("https://www.youtube.com/watch?v=N9777WExvCc");
         document.write("Aula de Pens. Comp.");
+         let tabuada =2;
+         let nome = " Victor";
 
         function meChame(){
-                alert("😐🤘");
+             document.write("<h1>Tabuada do " + Tabuada +  "</h1>");
+             document.write (tabuada + " x 1 = " +  (tabuada+1)+"<br>"); 
+             document.write (tabuada + " x 1 = " +  (tabuada+2)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+3)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+4)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+5)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+6)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+7)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+8)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+9)+"<br>");
+             document.write (tabuada + " x 1 = " +  (tabuada+10)+"<br>");
+             document.write("Feita por" + nome);
             }
