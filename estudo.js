f8f3727cae500a79d8bc1aea7ff66a607d3d242a
victorhugonegri <110132707+victorhugonegri@users.noreply.github.com>
2022-09-28 18:00:56 -0300

    28/09/22

---
+++ estudo.js
@@ -0,0 +1,7 @@
+alert("Oi, eu sou um alerta")
+        console.log("https://www.youtube.com/watch?v=N9777WExvCc");
+        document.write("Aula de Pens. Comp.");
+
+        function meChame(){
+                alert("😐🤘");
+            }
